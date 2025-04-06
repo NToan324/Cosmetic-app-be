b85962ae5f85062e639684cd4e927769a4473be5
NToan324 <nhattoan664t@gmail.com>
2025-04-06 15:33:34 +0700

    create customer and employee model

---
+++ src/models/customer.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { InferSchemaType, Schema } from 'mongoose'
+const customerSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      unique: true
+    },
+    role: {
+      type: String,
+      default: 'CUSTOMER'
+    },
+    rank: {
+      type: String,
+      enum: ['MEMBER', 'BRONZE', 'SILVER', 'GOLD', 'DIAMOND', 'PLATINUM']
+    },
+    point: {
+      type: Number,
+      default: 0
+    },
+    skin_type: {
+      type: String
+    },
+    skin_issues: {
+      type: String
+    },
+    age: {
+      type: Number
+    }
+  },
+  { timestamps: true }
+)
+
+const customer = mongoose.model('customer', customerSchema)
+export type Customer = InferSchemaType<typeof customerSchema>
+
+export default customer
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,22 +1,45 @@
 import { BadRequestError } from '@/core/error.response'
 import { CreatedResponse, OkResponse } from '@/core/success.response'
 import userModel from '@/models/user.model'
+import customerModel, { Customer } from '@/models/customer.model'
 class AuthService {
   async signup(payload: { phone: string; name: string }) {
+    const password = '123456'
     const { phone, name } = payload
     const isPhoneNumberExist = await userModel.exists({ phone })
+
     if (isPhoneNumberExist) {
       throw new BadRequestError('Phone number already exists')
     }
+
     const newUser = await userModel.create({
+      name,
       phone,
-      name
+      password
     })
-    return new CreatedResponse('User created successfully', newUser)
+
+    const newCustomer = (await customerModel.create({
+      userId: newUser._id,
+      role: 'CUSTOMER',
+      rank: 'MEMBER'
+    })) as Customer
+
+    const userResponse = {
+      id: newUser._id,
+      phone: newUser.phone,
+      name: newUser.name,
+      role: newCustomer.role,
+      rank: newCustomer.rank,
+      point: newCustomer.point
+    }
+
+    return new CreatedResponse('User created successfully', userResponse)
   }
+
   async login(data: { email: string; password: string }) {
     return new BadRequestError('Email already exists')
   }
+
   async deleteUser(id: string) {
     const user = await userModel.findByIdAndDelete(id)
     if (!user) {
@@ -24,6 +47,7 @@ class AuthService {
     }
     return new OkResponse('User deleted successfully', user)
   }
+
   async getUser(id: string) {
     const users = await userModel.findOne({ _id: id })
     return new OkResponse('Get all users successfully', users)
