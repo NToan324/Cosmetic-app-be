f55f13fd1e6ac0248ce96d42210923662343c4a1
NToan324 <nhattoan664t@gmail.com>
2025-04-07 00:18:56 +0700

    update

--- dist/middleware/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandler = (fn) => (req, res, next) => {
-    fn(req, res, next).catch(next);
-};
-exports.default = asyncHandler;
