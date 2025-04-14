d64852ae2bd1bbd097400dcb5f9cc3d3e6f1cf7c
NToan324 <nhattoan664t@gmail.com>
2025-04-14 20:56:20 +0700

    order

--- src/helpers/convertObjectId.ts
+++ src/helpers/convertObjectId.ts
@@ -2,3 +2,10 @@ import { ObjectId } from 'mongodb'
 export function convertToObjectId(id: string) {
   return new ObjectId(id)
 }
+
+export function generateOrderCode() {
+  const prefix = 'ORD'
+  const date = new Date().toISOString().slice(0, 10).replace(/-/g, '')
+  const random = Math.random().toString(36).substr(2, 5).toUpperCase() // ví dụ: 5 ký tự
+  return `${prefix}${date}${random}`
+}
